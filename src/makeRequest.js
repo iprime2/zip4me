55888687944ab8962be64f5b944d0b46bc7b3368
Sushil Gupta <kalwarsushil2@gmail.com>
2023-03-13 11:40:30 +0545

    Update makeRequest.js

--- src/makeRequest.js
+++ src/makeRequest.js
@@ -4,7 +4,7 @@ export const makeRequest = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
   headers: {
     //Authorization: 'bearer' + process.env.REACT_APP_API_TOKEN,
-    'Access-Control-Allow-Origin': 'https://sushil-krishil.on.fleek.co/',
+    'Access-Control-Allow-Origin': 'https://sushil-krishil.on.fleek.co',
     'Access-Control-Allow-Headers': 'content-type',
     'Access-Control-Allow-Methods': 'PUT, POST, GET, DELETE, PATCH, OPTIONS',
     'Access-Control-Allow-Credentials': true,
