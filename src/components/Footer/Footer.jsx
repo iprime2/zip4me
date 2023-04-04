0105791216550da3c835a263201a094e18ff5587
Sushil Gupta <kalwarsushil2@gmail.com>
2023-04-04 08:58:29 +0545

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -22,19 +22,13 @@ const Footer = () => {
         <div className='item'>
           <h1>About</h1>
           <span>
-            Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore. Lorem ipsum dolor sit
-            amet conse ctetur adipisicing elit, seddo eiusmod tempor incididunt
-            ut labore etdolore.
+            This site is under contruction you will be updated soon.
           </span>
         </div>
         <div className='item'>
           <h1>Contact</h1>
           <span>
-            Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore. Lorem ipsum dolor sit
-            amet conse ctetur adipisicing elit, seddo eiusmod tempor incididunt
-            ut labore etdolore.
+            You can contact piyus gupta for futher information.
           </span>
         </div>
       </div>
