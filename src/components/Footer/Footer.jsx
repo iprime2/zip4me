d727f7ca0e8a5994c0e009e4634871f6789368b9
Sushil Gupta <kalwarsushil2@gmail.com>
2023-06-14 17:45:08 +0545

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -25,9 +25,7 @@ const Footer = () => {
             Welcome to zip4me
 
 We aim to offer our customers a variety of the latest and Stylish Wearables. zip4me is The place to find the best Stylish Wearables for every taste and occasion. We thoroughly check the quality of our goods so that you only receive the best quality products. We offer all of this while providing excellent customer service and friendly support.
-
 We at Wear.Style believe in high quality and exceptional customer service. But most importantly, we believe shopping is a right, not a luxury, so we strive to deliver the best products at the most affordable prices, and ship them to you regardless of where you are located.
-
 That is why we have satisfied customers all over the world. The interests of our customers are always top priority for us, so we hope you will enjoy our products as much as we enjoy making them available to you.
           </span>
         </div>
