ff3d98bdc020ffd29947a6352b945c98828437b2
Sushil Gupta <kalwarsushil2@gmail.com>
2023-06-14 17:47:06 +0545

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -24,9 +24,11 @@ const Footer = () => {
           <span>
             Welcome to zip4me.
     <br />
-
 We aim to offer our customers a variety of the latest and Stylish Wearables. zip4me is The place to find the best Stylish Wearables for every taste and occasion. We thoroughly check the quality of our goods so that you only receive the best quality products. We offer all of this while providing excellent customer service and friendly support.
-We at Wear.Style believe in high quality and exceptional customer service. But most importantly, we believe shopping is a right, not a luxury, so we strive to deliver the best products at the most affordable prices, and ship them to you regardless of where you are located.
+We at Wear.Style believe in high quality and exceptional customer service.
+    <br />
+    But most importantly, we believe shopping is a right, not a luxury, so we strive to deliver the best products at the most affordable prices, and ship them to you regardless of where you are located.
+    <br />
 That is why we have satisfied customers all over the world. The interests of our customers are always top priority for us, so we hope you will enjoy our products as much as we enjoy making them available to you.
           </span>
         </div>
@@ -34,17 +36,23 @@ That is why we have satisfied customers all over the world. The interests of our
           <h1>Contact</h1>
           <span>
            Zip4me
-
+<br />
 eMail : piyushguptabrj@gmail.com
+    <br />
 Phone: +977-9845188342
-
+<br />
 Customer Care :
-
+<br />
 Please Allow 24 to 72 Hours Response Time
+    <br />
 Call us on +977-9845188342
+    <br />
 11am-5pm (Monday-Friday)
+  <br />
 Click To Connect on WhatsApp
+<br />
 11am-5pm (Monday-Friday)
+  <br />
           </span>
         </div>
       </div>
